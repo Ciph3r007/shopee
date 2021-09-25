1a8166258a4ab5d7114cc4d7edc4d8f339809003
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-09-25 16:46:19 +0600

    Connect API, add basic UI and navigation

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Redirect, Route, Switch } from "react-router-dom";
+import CartSideBar from "./components/CartSidebar";
+import NavBar from "./components/NavBar";
+import ProductListing from "./components/ProductListing";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+
+function App() {
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      name: "Basic Tee",
+      href: "#",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: "$35",
+      color: "Black",
+    },
+    // More products...
+  ]);
+
+  useEffect(() => {
+    async function getData() {
+      const { data } = await axios.get("https://fakestoreapi.com/products");
+      setProducts(data);
+      console.log("Data loaded");
+    }
+    getData();
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/not-found" component={NotFound} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <>
+              <CartSideBar />
+              <ProductListing products={products} />
+            </>
+          )}
+        />
+        <Redirect from="/home" to="/" />
+        <Redirect to="/not-found" />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
