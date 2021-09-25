70ee2899ef246ae7bdbcd84c59c4a1be44db00b8
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-09-26 00:26:28 +0600

    Fix grid layout

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -11,7 +11,10 @@ const ProductListing = ({ products, onIncrement, onDecrement }) => {
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <div key={product.id} className="relative">
+            <div
+              key={product.id}
+              className="relative flex-col flex justify-between"
+            >
               <div className="group">
                 <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none shadow-xl">
                   <img
@@ -42,14 +45,14 @@ const ProductListing = ({ products, onIncrement, onDecrement }) => {
                   <>
                     <button
                       onClick={() => onDecrement(product)}
-                      className="bg-blue-700 text-white p-2 rounded-lg shadow-sm cursor-pointer hover:bg-blue-400"
+                      className="bg-blue-700 text-white mx-2 my-2 px-6 py-2 rounded-lg shadow-sm cursor-pointer hover:bg-blue-400"
                     >
                       <MinusIcon className="h-3 w-3" aria-hidden="true" />
                     </button>
-                    <span className="mx-5">{product.inCart || 0}</span>
+                    <span className="mx-5 text-xl">{product.inCart || 0}</span>
                     <button
                       onClick={() => onIncrement(product)}
-                      className="bg-blue-700 text-white p-2 rounded-lg shadow-sm cursor-pointer hover:bg-blue-400"
+                      className="bg-blue-700 text-white mx-2 my-3 px-6 py-2 rounded-lg shadow-sm cursor-pointer hover:bg-blue-400"
                     >
                       <PlusIcon className="h-3 w-3" aria-hidden="true" />
                     </button>
